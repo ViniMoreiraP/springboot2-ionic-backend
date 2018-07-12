b35d9d44453706e717ff12d54c100d894403f173
ViniMoreiraP <viniciusmoreirap@hotmail.com>
2018-07-12 18:46:48 -0300

    Criando operacao de instanciacao

--- src/main/java/com/vinimoreira/cursomc/domain/Categoria.java
+++ src/main/java/com/vinimoreira/cursomc/domain/Categoria.java
@@ -1,5 +1,6 @@
 package com.vinimoreira.cursomc.domain;
 
+
 import java.io.Serializable;
 
 import javax.persistence.Entity;
@@ -10,52 +11,44 @@ import javax.persistence.Id;
 @Entity
 public class Categoria implements Serializable {
 	private static final long serialVersionUID = 1L;
+	
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
-	
 	private String nome;
 	
 	public Categoria() {
-		
 	}
 
-	
-	public Categoria(int id, String nome) {
+	public Categoria(Integer id, String nome) {
 		super();
 		this.id = id;
 		this.nome = nome;
 	}
 
-
-	public int getId() {
+	public Integer getId() {
 		return id;
 	}
 
-
-	public void setId(int id) {
+	public void setId(Integer id) {
 		this.id = id;
 	}
-
 
 	public String getNome() {
 		return nome;
 	}
 
-
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-
 
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + id;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
-
 
 	@Override
 	public boolean equals(Object obj) {
@@ -66,22 +59,12 @@ public class Categoria implements Serializable {
 		if (getClass() != obj.getClass())
 			return false;
 		Categoria other = (Categoria) obj;
-		if (id != other.id)
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
 			return false;
 		return true;
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
 }
