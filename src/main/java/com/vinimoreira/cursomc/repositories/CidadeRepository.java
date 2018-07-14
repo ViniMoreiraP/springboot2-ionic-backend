f05340aeec2e4599b8034b00a293107234f6f258
ViniMoreiraP <viniciusmoreirap@hotmail.com>
2018-07-14 12:22:42 -0300

    Estado e cidade

---
+++ src/main/java/com/vinimoreira/cursomc/repositories/CidadeRepository.java
@@ -0,0 +1,11 @@
+package com.vinimoreira.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.vinimoreira.cursomc.domain.Cidade;
+
+@Repository
+public interface CidadeRepository extends JpaRepository<Cidade, Integer> {
+
+}
