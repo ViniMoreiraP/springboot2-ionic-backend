b67510023b79f269246780799157bb620ff074b3
ViniMoreiraP <viniciusmoreirap@hotmail.com>
2018-07-16 17:15:03 -0300

    Endpoint /clientes/{id} disponivel

---
+++ src/main/java/com/vinimoreira/cursomc/services/ClienteService.java
@@ -0,0 +1,28 @@
+package com.vinimoreira.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.vinimoreira.cursomc.domain.Cliente;
+import com.vinimoreira.cursomc.repositories.ClienteRepository;
+import com.vinimoreira.cursomc.services.exceptions.ObjectNotFoundException;
+
+
+@Service 
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente find(Integer id) {	
+		Optional<Cliente> obj= repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+		
+	}
+	
+	
+
+}
