776bebe509cb0ca0e2e80584c395e902b7df06ff
ViniMoreiraP <viniciusmoreirap@hotmail.com>
2018-07-20 12:50:59 -0300

    Pedido, EstadoPagamento e Pagamento

--- src/main/java/com/vinimoreira/cursomc/domain/Cliente.java
+++ src/main/java/com/vinimoreira/cursomc/domain/Cliente.java
@@ -37,6 +37,9 @@ public class Cliente implements Serializable {
 	@CollectionTable(name="TELEFONE")
 	private Set<String> telefones= new HashSet<>();
 	
+	@OneToMany(mappedBy="cliente")
+	private List<Pedido> pedidos= new ArrayList<>();
+	
 	public Cliente () {}
 
 
@@ -120,6 +123,17 @@ public class Cliente implements Serializable {
 	}
 
 
+	public List<Pedido> getPedidos() {
+		return pedidos;
+	}
+
+
+	public void setPedidos(List<Pedido> pedidos) {
+		this.pedidos = pedidos;
+	}
+	
+	
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -145,7 +159,8 @@ public class Cliente implements Serializable {
 			return false;
 		return true;
 	}
-	
+
+
 	
 	
 	
